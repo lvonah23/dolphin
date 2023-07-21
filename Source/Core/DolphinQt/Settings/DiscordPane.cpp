e41e8b837ec330362a5fb7f4a40ad7ed606c7a34
lvonah23 <lwvonah@gmail.com>
2023-07-20 23:15:38 -0400

    added discord pane for settings widget

---
+++ Source/Core/DolphinQt/Settings/DiscordPane.cpp
@@ -0,0 +1,19 @@
+#include "DolphinQt/Settings/DiscordPane.h"
+
+#include <QCheckBox>
+#include <QComboBox>
+#include <QDateTimeEdit>
+#include <QFormLayout>
+#include <QGroupBox>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QRadioButton>
+#include <QSignalBlocker>
+#include <QSlider>
+#include <QVBoxLayout>
+#include <cmath>
+
+DiscordPane::DiscordPane()
+{
+
+}
